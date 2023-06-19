2d3c032951b58b8b5f8e85052d102584e7bf828e
forgeur35 <tesniere.matheo35@gmail.com>
2023-06-19 14:29:32 +0200

    first commit

--- app_non_super.py
+++ app_non_super.py
@@ -1,4 +1,11 @@
+import pandas as pd
+
+data = pd.read_csv("/content/drive/MyDrive/TP_IA/projet_IA/prepared_data.csv", index_col=0, header=0)
 
 
-#Reduction de dimension
+
+data_red = data.drop(columns=["num_veh", "id_usa", "an_nais", "descr_motif_traj", "ville", "date", "id_code_insee", "descr_cat_veh", "descr_agglo", "description_intersection", "age", "place", "descr_type_col", "region"])
+
+#pourcentage de perte :
+#(14/21)*100 = 66.66%
 
