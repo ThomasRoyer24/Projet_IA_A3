48c1614583ee0165ded86d493b0a8c72be35af19
forgeur35 <tesniere.matheo35@gmail.com>
2023-06-20 16:32:12 +0200

    visua test kmeans +

--- test_kmean.py
+++ test_kmean.py
@@ -11,10 +11,11 @@ coordinates = data[['longitude', 'latitude']]  # Extract longitude and latitude 
 coordinates_array = coordinates.to_numpy()
 
 # Specify the number of clusters (k)
-k = 3
+
+k = 13
 
 # Apply K-means algorithm
-kmeans = KMeans(n_clusters=1, max_iter=100)
+kmeans = KMeans(n_clusters=k, max_iter=100)
 kmeans.fit(coordinates_array)
 
 # Retrieve the cluster labels
@@ -23,31 +24,37 @@ cluster_labels = kmeans.labels_
 # Add cluster labels to the DataFrame
 data['cluster'] = cluster_labels
 
-plt.rcParams["figure.figsize"] = [7.50, 7.50]
-plt.rcParams["figure.autolayout"] = True
+# plt.rcParams["figure.figsize"] = [7.50, 7.50]
+# plt.rcParams["figure.autolayout"] = True
+#
+# tab_color = ['#FF0000',
+#              '#FF8800',
+#              '#FFFF00',
+#              '#FFFF88',
+#              '#888888',
+#              '#00FF00',
+#              '#00FF88',
+#              '#88FF00',
+#              '#00FFFF',
+#              '#0000FF',
+#              '#0088FF',
+#              '#8800FF',
+#              '#8888FF']
+#
+# for j in range(13):
+#   for i in range(len(data)):
+#     if (data['cluster'][i] == j):
+#       plt.plot( data['longitude'][i], data['latitude'][i], color=tab_color[j], marker='.', linestyle='')
+#   plt.plot(kmeans.cluster_centers_[j][0], kmeans.cluster_centers_[j][1], color="000000", marker='o', linestyle='')
+#   print("itr :", j)
 
-tab_color = ['#FF0000',
-             '#FF8800',
-             '#FFFF00',
-             '#FFFF88',
-             '#888888',
-             '#00FF00',
-             '#00FF88',
-             '#88FF00',
-             '#00FFFF',
-             '#0000FF',
-             '#0088FF',
-             '#8800FF',
-             '#8888FF']
+plt.scatter(
+    x=data['longitude'],
+    y=data['latitude'],
+    c=data['cluster']
+)
 
-for j in range(13):
-  for i in range(len(data)):
-    if (data['cluster'][i] == j):
-      plt.plot( data['longitude'][i], data['latitude'][i], color=tab_color[j], marker='.', linestyle='')
-  plt.plot(kmeans.cluster_centers_[j][0], kmeans.cluster_centers_[j][1], color="000000", marker='o', linestyle='')
-  print("itr :", j)
-
+for j in range(k):
+  plt.plot(kmeans.cluster_centers_[j][0], kmeans.cluster_centers_[j][1], color="#000000", marker='o', linestyle='')
 
 plt.show()
-
-
