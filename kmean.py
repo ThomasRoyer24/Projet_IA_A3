e8b78382eb779b555dfc38c719789a210fd9059e
BastienHuard <bastien.huard@isen-ouest.yncrea.fr>
2023-06-21 11:30:28 +0200

    visualisation of scores

--- kmean.py
+++ kmean.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_samples
 from sklearn import metrics
 from sklearn.metrics import davies_bouldin_score
+import random
+from sklearn.metrics import calinski_harabasz_score
+
+
 
 data = pd.read_csv("final_data.csv")
 
@@ -16,8 +20,8 @@ def kmean(distance, k, n):
   tab_cluster = [None] * len(data)
   temp = 99999999
   for i in range (k):
-    centro_lat.append(data["latitude"][i])
-    centro_long.append(data["longitude"][i])
+    centro_lat.append(random.choice(data["latitude"]))
+    centro_long.append(random.choice(data["longitude"]))
   for i in range(len(data)):
     for j in range (len(centro_lat)):
       if (distance == "L1"):
@@ -40,90 +44,89 @@ def kmean(distance, k, n):
       centro_long[i] = res_centroide[1]
   if n == 0:
     final = [tab_cluster, centro_lat, centro_long]
-    # print(tab_cluster)
-    # print(centro_lat)
-    # print(centro_long)
-    # tab_cluster_array = np.array(tab_cluster[:1000])
-    # silhouette = silhouette_score(data[['longitude', 'latitude']].head(1000), tab_cluster_array)
-    # print("Silhouette score:", silhouette)
-    #
-    # calinski = metrics.calinski_harabasz_score(data[['longitude', 'latitude']].head(1000), tab_cluster_array)
-    # print("Calinski-Harabasz score:", calinski)
-    #
-    # davies = davies_bouldin_score(data[['longitude', 'latitude']].head(1000), tab_cluster_array)
-    # print("Davies-Bouldin score:", davies)
-    #
-    # ch_scores = []
-    #
-    # # Define the range of values for k
-    # k_values = range(2, k + 1)
-    #
-    # # Iterate over each value of k
-    # for k in k_values:
-    #   # Perform K-means clustering and obtain the cluster labels
-    #
-    #   # Calculate the Calinski-Harabasz score
-    #   ch_score = metrics.calinski_harabasz_score(data[['longitude', 'latitude']].head(1000), tab_cluster_array)
-    #
-    #   # Append the score to the list
-    #   ch_scores.append(ch_score)
-    #
-    # # Plot the Calinski-Harabasz scores
-    # plt.plot(k_values, ch_scores, marker='o')
-    # plt.xlabel('Number of clusters (k)')
-    # plt.ylabel('Calinski-Harabasz Score')
-    # plt.title('Calinski-Harabasz Score for Different Values of k')
-    # plt.show()
+    tab_cluster_array = np.array(tab_cluster[:2000])
+    silhouette = silhouette_score(data[['longitude', 'latitude']].head(2000), tab_cluster_array)
+    print("Silhouette score:", silhouette)
+    silhouette_values = silhouette_samples(data[['longitude', 'latitude']].head(2000), tab_cluster_array)
 
-    # silhouette_values = silhouette_samples(data[['longitude', 'latitude']].head(1000), tab_cluster_array)
-    #
-    # # Get the number of unique clusters
-    # unique_labels = np.unique(tab_cluster_array)
-    # num_clusters = len(unique_labels)
-    #
-    # # Create a subplot with 1 row and 1 column
-    # fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-    #
-    # # Set y-axis limits and initialize the y_ticks variable
-    # y_lower, y_upper = 0, 0
-    # y_ticks = []
-    #
-    # # Loop over each cluster
-    # for i, label in enumerate(unique_labels):
-    #   # Get the silhouette scores for data points in the current cluster
-    #   cluster_silhouette_values = silhouette_values[tab_cluster_array == label]
-    #
-    #   # Sort the silhouette scores in ascending order
-    #   cluster_silhouette_values.sort()
-    #
-    #   # Calculate the size of the current cluster
-    #   cluster_size = cluster_silhouette_values.shape[0]
-    #
-    #   # Update y-axis limits and y_ticks
-    #   y_upper += cluster_size
-    #   y_ticks.append((y_lower + y_upper) / 2)
-    #
-    #   # Color the silhouette plot for the current cluster
-    #   color = plt.cm.get_cmap("Spectral")(i / num_clusters)
-    #   ax.fill_betweenx(np.arange(y_lower, y_upper), 0, cluster_silhouette_values, facecolor=color, alpha=0.7)
-    #
-    #   # Update y_lower for the next cluster
-    #   y_lower += cluster_size
-    #
-    # # Set the properties of the silhouette plot
-    # ax.axvline(x=silhouette_score(data[['longitude', 'latitude']].head(1000), tab_cluster_array), color="red", linestyle="--")
-    # ax.set_xlabel("Silhouette Coefficient")
-    # ax.set_ylabel("Cluster")
-    # ax.set_yticks(y_ticks)
-    # ax.set_title("Silhouette Plot")
-    # ax.grid(True)
-    #
-    # plt.show()
+    # Get the number of unique clusters
+    unique_labels = np.unique(tab_cluster_array)
+    num_clusters = len(unique_labels)
+
+    # Create a subplot with 1 row and 1 column
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+
+    # Set y-axis limits and initialize the y_ticks variable
+    y_lower, y_upper = 0, 0
+    y_ticks = []
+
+    # Loop over each cluster
+    for i, label in enumerate(unique_labels):
+      # Get the silhouette scores for data points in the current cluster
+      cluster_silhouette_values = silhouette_values[tab_cluster_array == label]
+
+      # Sort the silhouette scores in ascending order
+      cluster_silhouette_values.sort()
+
+      # Calculate the size of the current cluster
+      cluster_size = cluster_silhouette_values.shape[0]
+
+      # Update y-axis limits and y_ticks
+      y_upper += cluster_size
+      y_ticks.append((y_lower + y_upper) / 2)
+
+      # Color the silhouette plot for the current cluster
+      color = plt.cm.get_cmap("Spectral")(i / num_clusters)
+      ax.fill_betweenx(np.arange(y_lower, y_upper), 0, cluster_silhouette_values, facecolor=color, alpha=0.7)
+
+      # Update y_lower for the next cluster
+      y_lower += cluster_size
+
+    # Set the properties of the silhouette plot
+    ax.axvline(x=silhouette_score(data[['longitude', 'latitude']].head(2000), tab_cluster_array), color="red", linestyle="--")
+    ax.set_xlabel("Silhouette Coefficient")
+    ax.set_ylabel("Cluster")
+    ax.set_yticks(y_ticks)
+    ax.set_title("Silhouette Plot")
+    ax.grid(True)
+
+    plt.show()
     return final
   print("itération : ", n)
   return kmean(distance,k,n-1)
 
-  # print(tab_cluster)
+
+def davies(k):
+  davies_scores = []
+  for i in range(2, k + 1):
+    al = kmean("L1",i,3)
+    tab_cluster = al[0]
+    davies_score = davies_bouldin_score(data[['longitude', 'latitude']], tab_cluster)
+    davies_scores.append(davies_score)
+
+  # Plot the Davies-Bouldin scores
+  plt.plot(range(2, k + 1), davies_scores, marker='o')
+  plt.xlabel('Number of clusters (k)')
+  plt.ylabel('Davies-Bouldin Score')
+  plt.title('Davies-Bouldin Score for Different Values of k')
+  plt.show()
+
+def kalinski(k):
+  kalinski_scores = []
+  for i in range(2, k + 1):
+    print("TEST : ",i)
+    al = kmean("L1",i,3)
+    tab_cluster = al[0]
+    kalinski_score = calinski_harabasz_score(data[['longitude', 'latitude']], tab_cluster)
+    kalinski_scores.append(kalinski_score)
+
+  # Plot the Davies-Bouldin scores
+  plt.plot(range(2, k + 1), kalinski_scores, marker='o')
+  plt.xlabel('Number of clusters (k)')
+  plt.ylabel('Calinski-Harabasz Score')
+  plt.title('Calinski-Harabasz Score for Different Values of k')
+  plt.show()
+
 
 def mean_cluster(tab, cluster):
   temp_long = 0
@@ -141,7 +144,6 @@ def mean_cluster(tab, cluster):
   tab = [temp_lat,temp_long]
   return tab
 
-
 def dist_l1(long1, lat1, long2, lat2):
   return (abs(long2 - long1) + abs(lat2 - lat1))
 
@@ -152,8 +154,8 @@ def dist_haversine(long1, lat1, long2, lat2):
   return 2*math.asin((math.sqrt((math.sin((math.radians(lat1)-math.radians(long1))/2)*math.sin((math.radians(lat1)-math.radians(long1))/2)+math.cos(math.radians(lat1))*math.cos(math.radians(long1))*math.sin((math.radians(lat2)-math.radians(long2))/2))*math.sin((math.radians(lat2)-math.radians(long2))/2))))
 
 
-tab_km = kmean("L1",13,3)
-print(tab_km)
+# res = kmean("L1",13,5)
+kmean("L1", 13, 5)
 
 # plt.rcParams["figure.figsize"] = [7.50, 7.50]
 # plt.rcParams["figure.autolayout"] = True
--- test_kmean.py
+++ test_kmean.py
@@ -18,50 +18,50 @@ coordinates = data[['longitude', 'latitude']]  # Extract longitude and latitude 
 coordinates_array = coordinates.to_numpy()
 
 k_values = range(2, 15)  # Range of values for k
-
-davies_scores = []
-
-# Iterate over each value of k
-for k in k_values:
-    # Apply K-means algorithm
-    kmeans = KMeans(n_clusters=k, max_iter=5)
-    kmeans.fit(coordinates_array)
-
-    # Retrieve the cluster labels
-    cluster_labels = kmeans.labels_
-
-    # Calculate the Davies-Bouldin score
-    davies_score = davies_bouldin_score(coordinates_array, cluster_labels)
-
-    # Append the score to the list
-    davies_scores.append(davies_score)
-
-# Plot the Davies-Bouldin scores
-plt.plot(k_values, davies_scores, marker='o')
-plt.xlabel('Number of clusters (k)')
-plt.ylabel('Davies-Bouldin Score')
-plt.title('Davies-Bouldin Score for Different Values of k')
-plt.show()
-
-
-# k = 13
-# # Apply K-means algorithm
-# kmeans = KMeans(n_clusters=k, max_iter=5)
-# kmeans.fit(coordinates_array)
 #
-# # Retrieve the cluster labels
-# cluster_labels = kmeans.labels_
+# davies_scores = []
 #
-# # Add cluster labels to the DataFrame
-# data['cluster'] = cluster_labels
+# # Iterate over each value of k
+# for k in k_values:
+#     # Apply K-means algorithm
+#     kmeans = KMeans(n_clusters=k, max_iter=5)
+#     kmeans.fit(coordinates_array)
 #
-# silhouette_avg = silhouette_score(coordinates_array, cluster_labels)
-# calinski_score = calinski_harabasz_score(coordinates_array, cluster_labels)
-# davies_bouldin = davies_bouldin_score(coordinates_array, cluster_labels)
+#     # Retrieve the cluster labels
+#     cluster_labels = kmeans.labels_
 #
-# print("Silhouette score:", silhouette_avg)
-# print("Calinski-Harabasz score:", calinski_score)
-# print("Davies-Bouldin score:", davies_bouldin)
+#     # Calculate the Davies-Bouldin score
+#     davies_score = davies_bouldin_score(coordinates_array, cluster_labels)
+#
+#     # Append the score to the list
+#     davies_scores.append(davies_score)
+#
+# # Plot the Davies-Bouldin scores
+# plt.plot(k_values, davies_scores, marker='o')
+# plt.xlabel('Number of clusters (k)')
+# plt.ylabel('Davies-Bouldin Score')
+# plt.title('Davies-Bouldin Score for Different Values of k')
+# plt.show()
+#
+
+k = 13
+# Apply K-means algorithm
+kmeans = KMeans(n_clusters=k, max_iter=5)
+kmeans.fit(coordinates_array)
+
+# Retrieve the cluster labels
+cluster_labels = kmeans.labels_
+
+# Add cluster labels to the DataFrame
+data['cluster'] = cluster_labels
+
+silhouette_avg = silhouette_score(coordinates_array, cluster_labels)
+calinski_score = calinski_harabasz_score(coordinates_array, cluster_labels)
+davies_bouldin = davies_bouldin_score(coordinates_array, cluster_labels)
+
+print("Silhouette score:", silhouette_avg)
+print("Calinski-Harabasz score:", calinski_score)
+print("Davies-Bouldin score:", davies_bouldin)
 
 
 #
@@ -92,53 +92,53 @@ plt.show()
 # plt.ylabel('Calinski-Harabasz Score')
 # plt.title('Calinski-Harabasz Score for Different Values of k')
 # plt.show()
+silhouette_values = silhouette_samples(coordinates_array, cluster_labels)
 
-# silhouette_values = silhouette_samples(coordinates_array, cluster_labels)
-#
-# # Get the number of unique clusters
-# unique_labels = np.unique(cluster_labels)
-# num_clusters = len(unique_labels)
-#
-# # Create a subplot with 1 row and 1 column
-# fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-#
-# # Set y-axis limits and initialize the y_ticks variable
-# y_lower, y_upper = 0, 0
-# y_ticks = []
-#
-# # Loop over each cluster
-# for i, label in enumerate(unique_labels):
-#     # Get the silhouette scores for data points in the current cluster
-#     cluster_silhouette_values = silhouette_values[cluster_labels == label]
-#
-#     # Sort the silhouette scores in ascending order
-#     cluster_silhouette_values.sort()
-#
-#     # Calculate the size of the current cluster
-#     cluster_size = cluster_silhouette_values.shape[0]
-#
-#     # Update y-axis limits and y_ticks
-#     y_upper += cluster_size
-#     y_ticks.append((y_lower + y_upper) / 2)
-#
-#     # Color the silhouette plot for the current cluster
-#     color = plt.cm.get_cmap("Spectral")(i / num_clusters)
-#     ax.fill_betweenx(np.arange(y_lower, y_upper), 0, cluster_silhouette_values, facecolor=color, alpha=0.7)
-#
-#     # Update y_lower for the next cluster
-#     y_lower += cluster_size
-#
-# # Set the properties of the silhouette plot
-# ax.axvline(x=silhouette_avg, color="red", linestyle="--")
-# ax.set_xlabel("Silhouette Coefficient")
-# ax.set_ylabel("Cluster")
-# ax.set_yticks(y_ticks)
-# ax.set_title("Silhouette Plot")
-# ax.grid(True)
-#
-# plt.show()
+# Get the number of unique clusters
+unique_labels = np.unique(cluster_labels)
+num_clusters = len(unique_labels)
 
+# Create a subplot with 1 row and 1 column
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
 
+# Set y-axis limits and initialize the y_ticks variable
+y_lower, y_upper = 0, 0
+y_ticks = []
+
+# Loop over each cluster
+for i, label in enumerate(unique_labels):
+    # Get the silhouette scores for data points in the current cluster
+    cluster_silhouette_values = silhouette_values[cluster_labels == label]
+
+    # Sort the silhouette scores in ascending order
+    cluster_silhouette_values.sort()
+
+    # Take only the first 2000 values
+    cluster_silhouette_values = cluster_silhouette_values[:2000]
+
+    # Calculate the size of the current cluster
+    cluster_size = cluster_silhouette_values.shape[0]
+
+    # Update y-axis limits and y_ticks
+    y_upper += cluster_size
+    y_ticks.append((y_lower + y_upper) / 2)
+
+    # Color the silhouette plot for the current cluster
+    color = plt.cm.get_cmap("Spectral")(i / num_clusters)
+    ax.fill_betweenx(np.arange(y_lower, y_upper), 0, cluster_silhouette_values, facecolor=color, alpha=0.7)
+
+    # Update y_lower for the next cluster
+    y_lower += cluster_size
+
+# Set the properties of the silhouette plot
+ax.axvline(x=silhouette_avg, color="red", linestyle="--")
+ax.set_xlabel("Silhouette Coefficient")
+ax.set_ylabel("Cluster")
+ax.set_yticks(y_ticks)
+ax.set_title("Silhouette Plot")
+ax.grid(True)
+
+plt.show()
 
 
 
